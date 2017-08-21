5d93ce4d323d8a26dbecfa876558064f28aa325f
Tahir Naushasd <t.naushad25@gmail.com>
2017-08-21 12:40:25 +0100

    asp.net core 2 file providers

---
+++ Fiver.Asp.FileProviders/UseMiddlewareExtensions.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiver.Asp.FileProviders
+{
+    public static class UseMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHelloFileProvider(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<HelloFileProviderMiddleware>();
+        }
+    }
+
+    //public class HelloFileProviderMiddleware
+    //{
+    //    private readonly RequestDelegate next;
+    //    private readonly IFileProvider fileProvider;
+
+    //    public HelloFileProviderMiddleware(
+    //        RequestDelegate next,
+    //        IFileProvider fileProvider)
+    //    {
+    //        this.next = next;
+    //        this.fileProvider = fileProvider;
+    //    }
+
+    //    public async Task Invoke(HttpContext context)
+    //    {
+    //        var output = new StringBuilder("");
+
+    //        IDirectoryContents dir = this.fileProvider.GetDirectoryContents("");
+    //        foreach (IFileInfo item in dir)
+    //        {
+    //            output.AppendLine(item.Name);
+    //        }
+
+    //        await context.Response.WriteAsync(output.ToString());
+    //    }
+    //}
+
+    public class HelloFileProviderMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IFileProvider fileProvider;
+
+        public HelloFileProviderMiddleware(
+            RequestDelegate next,
+            IFileProvider fileProvider)
+        {
+            this.next = next;
+            this.fileProvider = fileProvider;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            IFileInfo file = this.fileProvider.GetFileInfo("Startup.cs");
+
+            using (var stream = file.CreateReadStream())
+            using (var reader = new StreamReader(stream))
+            {
+                var output = await reader.ReadToEndAsync();
+                await context.Response.WriteAsync(output.ToString());
+            }
+        }
+    }
+}
